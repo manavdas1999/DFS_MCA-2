a011eb0149b1c28995c0d06005411e5063dc1031
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-01-31 09:57:48 +0530

    Create 31_1_24.c

---
+++ 31_1_24.c
@@ -0,0 +1,30 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int secondLargest(int* arr, int len){
+    // STEP1: find largest
+    int largest = arr[0]; 
+    for(int i=1; i<len; i++){
+        if(largest < arr[i]){
+            largest = arr[i];
+        }
+    }
+    printf("%d", largest);
+    
+    // STEP2: find next largest
+    int nextLargest = arr[0];
+    for(int i=1; i<len; i++){
+        if(nextLargest < arr[i] && arr[i] != largest){
+            nextLargest = arr[i];
+        }
+    }
+    return nextLargest;
+}
+
+
+int main() {
+    int* arr[5] = {3,5,1,6,7};
+    int c = secondLargest(arr, 5);
+    printf("%d", c);
+    return 0;
+}
