0f5a44e9d3fd9395cb1a1f0938219685e3c3b4a7
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-02-04 10:01:16 +0530

    Update 2_2_24.c

--- 2_2_24.c
+++ 2_2_24.c
@@ -6,6 +6,10 @@ Q1. Check whether a given matrix is sparse or not.
 Q2. Convert a given sparse matrix into coordinate set/3-row triplet representation.
 */
 
+/*
+Points of struggle:
+1. how to pass and return matrix to/from a function in c.
+*/
 
 #include <stdio.h>
 
@@ -29,11 +33,22 @@ int isSparseMatrix(int n, int arr[][n]){
     return (countZero > (n*n)/2) ? 1 : 0;
 }
 
+int elementCount(int n, int arr[][n]){
+    int count =0;
+    for(int i=0; i<n*n; i++){
+        if(arr[i%n][i/5] != 0) count++; 
+    }
+    return count;
+}
+
 int main() {
     int arr[5][5] = {{5,0,8,0,0}, {0,0,0,9,0}, {0,2,0,0,6}, {0,0,0,1,0}, {0,0,7,0,0}};
     
     printMatrix(5, arr);
     printf("is given matrix is Sparse matrix? %s", isSparseMatrix(5, arr) ? "true" : "false");
+
+    int nonZeroCount = elementCount(5, arr);
+    printf("Count of Non zero elements: %d", nonZeroCount);
 
     return 0;
 }
