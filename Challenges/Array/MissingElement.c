e0fcada306f9321b6e7e23da35bd83ec5a382c98
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-02-21 10:11:43 +0530

    Create MissingElement.c

---
+++ Challenges/Array/MissingElement.c
@@ -0,0 +1,4 @@
+// Find the missing element (single or multiple) from sorted/unsorted array
+
+// Q1. Find single missing element from sorted/unsorted array containing n natural numbers starting from 1.
+// Q2. Find single missing element from sorted array containing containing n natural numbers starting from any value a.
