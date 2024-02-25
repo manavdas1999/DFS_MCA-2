d3b231ab693101b680dea69b5ba5de5f2cf51248
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-02-25 10:18:27 +0530

    Update linkedList.c

--- myNotes/linkedList.c
+++ myNotes/linkedList.c
@@ -43,6 +43,98 @@ void display(struct Node *head){
     printf("\n");
 }
 
+int count(struct Node *head){
+    int count = 0;
+    struct Node *p = head;
+    while(p != NULL){
+        count++;
+        p= p->next;
+    } 
+    return count; //for the head node
+}
+
+int sum(struct Node *head){
+    int sum=0;
+    struct Node *p = head;
+    while(p != NULL){
+        sum += p->data;
+        p = p->next;
+    }
+    return sum;
+}
+
+int max(struct Node *head){
+    int max = 0;
+    struct Node *p = head;
+    while(p != NULL){
+        if(max < p->data) max = p->data;
+        p = p->next;
+    }
+    return max;
+}
+
+int min(struct Node *head){
+    int min = 100;  // some large value
+    struct Node *p = head;
+    while(p != NULL){
+        if(min > p->data) min = p->data;
+        p = p->next;
+    }
+    return min;
+}
+
+// only linear search works for LL
+struct Node* search(struct Node *head, int value){
+    struct Node *p = head;
+    while(p != NULL){
+        if(p->data == value) return p; //returning address
+        p = p->next;
+    }
+    return NULL; // not found
+}
+
+void append(struct Node *head, int value){
+    struct Node *p = head;
+    while(p->next != NULL){
+        p=p->next;
+    }
+    p->next = add(value);
+}
+
+// insert at first is not working for some reason
+void insertAtFirst(struct Node *head, int value){
+    struct Node *temp = add(value);
+    temp->next = head;
+    head = temp;
+}
+
+
+void insert(struct Node *head, int pos, int value){
+    // insert at first
+    if(pos == 0){
+        // struct Node *temp = add(value);
+        // temp->next = head;
+        // head = temp;
+        // display(head);
+
+        return;
+    }
+    else{
+        // insert at any other
+        struct Node *p = head;
+        for(int i=1; i<=pos-2; i++){
+            if(p == NULL) {
+                printf("\nInvalid Position.\n");
+                break;
+            }
+            p = p->next;
+        }
+        struct Node *temp = add(value);
+        temp->next = p->next;
+        p->next = temp;
+    }
+}
+
 int main() {
     
     // intialization of head
@@ -61,6 +153,26 @@ int main() {
     // printf("%d", head->next->data);
     
     display(head);
+    printf("%d\n", count(head));
+    printf("%d\n", sum(head));
+    printf("%d\n", max(head));
+    printf("%d\n", min(head));
+    
+    struct Node *myElement = search(head, 3);
+    printf("%d\n", myElement->data);
+    
+    insert(head, 2, 22);
+    append(head, 11);
+    insertAtFirst(head, 12);
+     display(head);
+
+    
+        // struct Node *t = head; //both point ar first node
+    // while(t != NULL){
+    //     printf("%d ", t->data);
+    //     t = t->next; // moving forward
+    // }
+
     
     return 0;
 }
