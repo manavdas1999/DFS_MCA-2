28bb3f5b6452a1600f1e6cd546346b1610ee1cf0
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-02-28 10:19:11 +0530

    Update circularLinkedList.c

--- myNotes/circularLinkedList.c
+++ myNotes/circularLinkedList.c
@@ -33,8 +33,35 @@ struct Node* createFromArray(int arr[], int size){
     return head;
 }
 
+void display(struct Node* head){
+    struct Node* temp = head;
+    do{
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }while(temp != head);
+    printf("\n");
+}
+
 int main() {
  
-
+    struct Node *head = NULL;
+    // // manual 
+    // head = createNode(10);
+    // head->next = head; // one node CLL
+    // printf("%d", head->data);
+    
+    // // adding more nodes
+    // head->next = createNode(20);
+    // head->next->next = head; // two node CLL
+    // printf("%d", head->next->data);
+    int values[] = {1,2,3,4};
+    head = createFromArray(values, 4);
+    display(head);
     return 0;
 }
+
+
+
+
+
+
