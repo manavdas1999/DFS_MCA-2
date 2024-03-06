c8f6a6d7e6dc8b8b50de74fa0387058a865ba385
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-03-06 19:23:52 +0530

    Update stackADT.c

--- myNotes/stackADT.c
+++ myNotes/stackADT.c
@@ -28,11 +28,54 @@ void push(struct Stack* st, int value){
     }
 }
 
+int pop(struct Stack* st){
+    if(st->top == -1){
+        printf("\nStack is empty!\n");
+        return -1;
+    }
+    return st->data[st->top--];
+}
+
+int peek(struct Stack *st){
+    if(st->top == -1){
+        printf("\nStack is empty!\n");
+        return -1;
+    }
+    return st->data[st->top];
+}
+
+int count(struct Stack *st){
+    return st->top+1;
+}
+
+int max(struct Stack *st){
+    if(st->top == -1){
+        printf("\nStack is empty!\n");
+        return -1;
+    }
+    int max = st->data[0];
+    for(int i = 1; i <= st->top; i++){
+        if(max < st->data[i]) max = st->data[i];
+    }
+    return max;
+}
+
+int min(struct Stack *st){
+    if(st->top == -1){
+        printf("\nStack is empty!\n");
+        return -1;
+    }
+    int min = st->data[0];
+    for(int i = 1; i <= st->top; i++){
+        if(min > st->data[i]) min = st->data[i];
+    }
+    return min;
+}
 
 void display(struct Stack* st){
     if(st->top == -1) printf("\nStack is empty\n");
     else{
-        for(int i=0; i<=st->top; i++){
+        for(int i=st->top; i>=0; i--){
             printf("%d ", st->data[i]);
         }
         printf("\n");
@@ -48,8 +91,18 @@ int main() {
     push(&myStack, 30);
     push(&myStack, 40);
     
+    printf("top Item: %d \n",peek(&myStack));
+    printf("current max item: %d \n",max(&myStack));
+    printf("current min item: %d \n",min(&myStack));
+    
     display(&myStack);
     
+    printf("popped Item: %d \n",pop(&myStack));
+    printf("popped Item: %d \n",pop(&myStack));
+    
+    display(&myStack);
+    
+    printf("current item count: %d \n",count(&myStack));
     
     return 0;
 }
