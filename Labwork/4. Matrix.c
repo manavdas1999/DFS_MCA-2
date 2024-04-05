bfab604a682043b1198a3d586568af880310c3c7
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-05 17:58:24 +0530

    Create 4. Matrix.c

---
+++ Labwork/4. Matrix.c
@@ -0,0 +1,12 @@
+/*
+Matrix ADT
+Write A C Program to Perform Following Operation on Matrix 
+1.	Addition Of Two Matrix.
+2.	Subtraction Of Two Matrix.
+3.	Multiplication Of Two Matrix.
+4.	Transpose Of Two Matrix.
+5.	Diagonal Sum of Two Matrix (row and col must be same.
+6.	Check If Matrix Is Identity Or not.
+7.	Exit.
+
+*/
