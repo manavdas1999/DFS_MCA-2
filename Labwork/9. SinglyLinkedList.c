e08494895451b8b26a9fd5d6d89e162889f2b110
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-05 21:56:21 +0530

    Create 9. SinglyLinkedList.c

---
+++ Labwork/9. SinglyLinkedList.c
@@ -0,0 +1,16 @@
+/*
+9. Write a program to following operations on singly linked list
+
+1.	Addition of a new node when list is empty.
+2.	 Addition of a node at the end of the list.
+3.	 Addition of a node at the beginning of linked list.
+4.	 Addition of a node at the specific position.
+5.	 Traversing a linked list.
+6.	Counting number of nodes in linked list.
+7.	 Deleting a node from the linked list by specifying the value of the deleting node. 
+8.	Reverse A linked List.
+9.	Sorting.
+10.	Search
+11.	Insert a node in a sorted linked list such that linked list remains sorted
+*/
+
