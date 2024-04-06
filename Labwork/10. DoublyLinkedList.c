863591c11bc1bdaacdba78a9a6777812cb30386d
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-04-06 09:38:12 +0530

    Create 10. DoublyLinkedList.c

---
+++ Labwork/10. DoublyLinkedList.c
@@ -0,0 +1,14 @@
+/*
+10. Write a program to following operations on Doubly linked list
+
+1.	Addition of a new node when list is empty.
+2.	Addition of a node at the end of the list.
+3.	Addition of a node at the beginning of linked list.
+4.	Addition of a node at the specific position.
+5.	Traversing a linked list.
+6.	Counting number of nodes in linked list.
+7.	Deleting a node from the linked list at beginning, at end, at specific position. 
+8.	Reverse A linked List.
+9.	Sorting
+10.	Search
+*/
