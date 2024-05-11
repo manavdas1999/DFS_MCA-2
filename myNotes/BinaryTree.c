0962ff64f4b9cae86d14c07bd0903459ea1428cc
manavdas1999 <61658813+manavdas1999@users.noreply.github.com>
2024-05-11 17:24:57 +0530

    Update BinaryTree.c

--- myNotes/BinaryTree.c
+++ myNotes/BinaryTree.c
@@ -6,7 +6,7 @@ struct Node {
     struct Node *left, *right;
 };
 
-// Pre-order traversal creation
+// pre-order traversal creation
 struct Node* createPreOrder(){
     int input;
     printf("\nEnter data for node (-1 if no node):");
@@ -15,26 +15,52 @@ struct Node* createPreOrder(){
     if(input == -1){
         return NULL;
     }
-
-    // Parent
+    
     struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
     newNode->data = input;
-
-    // Left Child 
+    
     printf("\nLeft Node of %d: ", newNode->data);
-    newNode->left = create();
-
-    // Right Child 
+    newNode->left = createPreOrder();
     printf("\nRight Node of %d: ", newNode->data);
-    newNode->right = create();
+    newNode->right = createPreOrder();
     
     return newNode;
 }
 
+void preOrderTraversal(struct Node* temp){
+    if(temp != NULL){
+        printf("%d ", temp->data);
+        preOrderTraversal(temp->left);
+        preOrderTraversal(temp->right);
+    }
+}
+
+void inOrderTraversal(struct Node* temp){
+    if(temp != NULL){
+        inOrderTraversal(temp->left);
+        printf("%d ", temp->data);
+        inOrderTraversal(temp->right);
+    }
+}
+
+void postOrderTraversal(struct Node* temp){
+    if(temp != NULL){
+        postOrderTraversal(temp->left);
+        postOrderTraversal(temp->right);
+        printf("%d ", temp->data);
+    }
+}
 
 int main() {
     struct Node* root;
     root = createPreOrder();
+    
+    preOrderTraversal(root);
+    printf("\n");
+    inOrderTraversal(root);
+    printf("\n");
+    postOrderTraversal(root);
+    
 
     return 0;
 }
